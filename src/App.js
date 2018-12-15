76b82cc27ae9b738ed341f2d2e928d85aa621460
Michael Morgan <michael1986morgan@gmail.com>
2018-12-14 23:44:52 -0500

    init app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
-class App extends Component {
+class Reddit extends Component {
+  state = {
+    posts: [],
+    author: "",
+    score: null
+  }
+
+  componentDidMount() {
+    axios.get('https://www.reddit.com/r/reactjs.json')
+    .then(res => {
+      const posts = res.data.data.children.map( obj => obj.data);
+      this.setState({ posts })
+    })
+  }
+      
   render() {
+    const { posts } = this.state;
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>/r/reactjs</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}><a>{post.score} - <strong>{post.title}</strong></a> by {post.author}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+export default Reddit;
